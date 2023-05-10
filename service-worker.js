b841615e303cc9356cae56df4fe06c0b5453c83d
Ahn jisoo <anjisu2001@naver.com>
2023-05-10 12:21:16 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nwitter/index.html/precache-manifest.96493801905baad9d5b612a89793801e.js"
+  "/nwitter/precache-manifest.f0faa58a8017ae2bb24760fded92dda3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nwitter/index.html/index.html", {
+workbox.routing.registerNavigationRoute("/nwitter/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
