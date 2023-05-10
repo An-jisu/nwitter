0444cc9ff15bb0340af4087664e631cfc2832edd
Ahn jisoo <anjisu2001@naver.com>
2023-05-10 11:57:08 +0900

    Updates

---
+++ precache-manifest.f0faa58a8017ae2bb24760fded92dda3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cae0b86999b35b332839",
+    "url": "/nwitter/static/js/main.dad5a659.chunk.js"
+  },
+  {
+    "revision": "b45e4dffb5d75f7371d3",
+    "url": "/nwitter/static/js/runtime~main.0fcaf8df.js"
+  },
+  {
+    "revision": "532c37b4e8640f52363f",
+    "url": "/nwitter/static/js/2.dfca0547.chunk.js"
+  },
+  {
+    "revision": "86c177118cf92e6f68f97a3ce6b79189",
+    "url": "/nwitter/index.html"
+  }
+];
